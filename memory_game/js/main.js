58870ac8cb75b7793c9941316f57620b084daba8
Brendan Weinstein <bmweinstein7@gmail.com>
2017-08-23 13:07:05 -0400

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,9 +1,30 @@
+console.log('Up and running!');
 
-
- var cardOne = "queen";
- var cardTwo = "queen";
- var cardThree = "king";
- var cardFour = "king";
+console.log("user flipped" + cardOne);
  
-console.log("User flipped " + "" + cardThree);
-console.log("User flipped " + "" + cardTwo);
+ var cards = ["queen", "queen", "king", "king"];
+ var cardsInPlay = [];
+ var cardOne = cards[0];
+ 
+ cardsInPlay.push(cardOne);
+ 
+ console.log("user flipped " + cardOne);
+ 
+ var cardTwo = cards[1];
+ cardsInPlay.push(cardTwo);
+ 
+ console.log("user flipped " + cardTwo);
+ 
+ 
+ 
+if (cardsInPlay.length === 2) //if statement
+ {
+ console.log("You Selected 2 Cards");
+
+ if (cardsInPlay[0] === cardsInPlay[1]) {
+
+    alert("You found a match!");
+  } else {
+   alert("Sorry, try again");
+  }
+}
